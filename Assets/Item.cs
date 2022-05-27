bc54e4700ab1246b7e23f4c27d2e321d2c8f0000
tqut214 <tqut214@gmail.com>
2022-05-28 00:55:37 +0700

    Finish add_logic_show_inventory

---
+++ Assets/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item 
+{
+    public int id;
+    public string name;
+    public string spriteName;
+    public int quantity;
+}
